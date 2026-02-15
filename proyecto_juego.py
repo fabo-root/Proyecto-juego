fdb073104e3b344fef53a341a4b848eca1d7e867
faboo-root <fabribombon97@gmail.com>
2026-02-15 13:26:39 -0500

    Actualice documentacion de codigo

--- proyecto_juego.py
+++ proyecto_juego.py
@@ -1,4 +1,6 @@
+# La biblioteca random pe permite realizar operaciones alteatorias
 import random
+# Mantiene activo el menu 
 while True:
     print("----------BIENVENIDO  AL JUEGO PIEDRA, PAPEL  Y TIJERA---------------")
     print("1. Jugar")
@@ -10,27 +12,32 @@ while True:
     if menu == "1":
         print("-----------------------------------------------------")
         opciones = ["piedra", "papel", "tijera"]
-        # pedir el numero de partidas
+        # valida el numero de partidas
         while True:
+            # Se utilixo el metodo .sttrrip()
+            # ya que me permite borrar espacios en blanco 
             partidas = input(" -----Cuantas partidas desea jugar ?-----: ").strip()
-            #metodo isdigit()  verifica si todos los caracteres de la cadena son dígitos numéricos
+            # Metodo isdigit()  verifica si todos los caracteres 
+            # de la cadena son dígitos numéricos
             if partidas.isdigit() and int(partidas) > 0:
                 partidas = int(partidas)
                 break
             else:
                 print("Ingrese un número válido.")
         # ciclo de partidas
+        # La funcion range repite el juego el numero de veces que elijio el usuario
         for ronda in range(1, partidas + 1):
             print(f"\n--- Partida {ronda} ---")
 
-            # pedir jugada válida
+            # Valida piedra, papel, tijera 
             while True:
+                # el metodo .lower() convierte las cadenas string mayusculas a minusulas
                 usuario = input("Elige piedra, papel o tijera: ").lower().strip()
                 if usuario in opciones:
                     break
                 else:
                     print("Opción incorrecta")
-
+            # seleccionar un elemento aleatorio de la lista
             computadora = random.choice(opciones)
 
             print("Usuario eligió:", usuario)
